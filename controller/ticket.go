cea3c4515482831206295d8c6493f95b56fc1ad4
zachary <zachary@gastom.com.tw>
2024-03-01 17:16:03 +0800

    feat: create and update tickets.

--- controller/ticket.go
+++ controller/ticket.go
@@ -2,28 +2,102 @@ package controller
 
 import (
 	"net/http"
+	"ticketsale/model"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateTicket(ctx *gin.Context) {
 	type form struct {
-		Name string `json:"name"`
+		Name      string `json:"name"`
 		Introduce string `json:"introduce"`
-		Count int `json:"count"`
+		Count     int    `json:"count"`
 		ExpiresAt string `json:"expires_at"`
 	}
 	var req form
+	var model model.Tickets
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
-			"msg": "Invalid form",
+			"msg":   "Invalid form",
 			"error": err.Error(),
 		})
 		return
 	}
+	model.Name = req.Name
+	model.Introduce = req.Introduce
+	model.Count = req.Count
+	timeLayout := "2006-01-02 15:04:05"
+	expireAt, err := time.Parse(timeLayout, req.ExpiresAt)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"msg":   "Invalid time format",
+			"error": err.Error(),
+		})
+		return
+	}
+	model.ExpiresAt = expireAt
+	err = model.Create()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"msg":   "Create ticket failed",
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "Create ticket success",
+	})
 }
 
 func UpdateTicket(ctx *gin.Context) {
+	id := ctx.Param("id")
+	type form struct {
+		Count int `json:"count"`
+		Introduce string `json:"introduce"`
+		ExpiresAt string `json:"expires_at"`
+	}
+	var req form
+	var model model.Tickets
+	_, err :=  model.Find(map[string]interface{}{"id": id})
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"msg":   "Ticket not found",
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"msg":   "Invalid form",
+			"error": err.Error(),
+		})
+		return
+	}
+	model.Count = req.Count
+	model.Introduce = req.Introduce
+	timeLayout := "2006-01-02 15:04:05"
+	expireAt, err := time.Parse(timeLayout, req.ExpiresAt)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"msg":   "Invalid time format",
+			"error": err.Error(),
+		})
+		return
+	}
+	model.ExpiresAt = expireAt
+	err = model.Update()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"msg":   "Update ticket failed",
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "Update ticket success",
+	})
 
 }
 
--- model/tickets.go
+++ model/tickets.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"ticketsale/config"
 	"time"
 
 	"gorm.io/gorm"
@@ -9,6 +10,7 @@ import (
 type Tickets struct {
 	gorm.Model
 	OwnerID   int
+	Name      string `gorm:"unique"`
 	Count     int
 	Introduce string `gorm:"type:text"`
 	ExpiresAt time.Time
@@ -30,3 +32,63 @@ type TicketsSalePrice struct {
 	Tickets       Tickets `gorm:"foreignKey:TicketsID"`
 	TicketsType   TicketsType `gorm:"foreignKey:TicketsTypeID"`
 }
+
+// ticket's method
+func (t *Tickets) Find(params map[string]interface{}) (ret Tickets, err error) {
+	result := config.DB.Where(params).First(&ret)
+	if result.RowsAffected == 0 {
+		err = result.Error
+	}
+	if result.Error != nil {
+		err = result.Error
+	}
+	return
+}
+
+func (t *Tickets) Create() error {
+	err := config.DB.Create(t)
+	return err.Error
+}
+
+func (t *Tickets) Update() error {
+	err := config.DB.Save(t)
+	return err.Error
+}
+
+func (t *Tickets) Delete() error {
+	err := config.DB.Delete(t)
+	return err.Error
+}
+
+// tickets type's method
+func (t *TicketsType) Create() error {
+	err := config.DB.Create(t)
+	return err.Error
+}
+
+func (t *TicketsType) Update() error {
+	err := config.DB.Save(t)
+	return err.Error
+}
+
+func (t *TicketsType) Delete() error {
+	err := config.DB.Delete(t)
+	return err.Error
+}
+
+// tickets sale price's method
+func (t *TicketsSalePrice) Create() error {
+	err := config.DB.Create(t)
+	return err.Error
+}
+
+func (t *TicketsSalePrice) Update() error {
+	err := config.DB.Save(t)
+	return err.Error
+}
+
+func (t *TicketsSalePrice) Delete() error {
+	err := config.DB.Delete(t)
+	return err.Error
+}
+
