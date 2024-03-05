03f58fea7dcc0e7abd1f9a8178bf7393aa5dd6db
zachary <zachary@gastom.com.tw>
2024-03-05 16:05:54 +0800

    feat: route for ticket, ticketType, ticketSalePrice

--- route/route.go
+++ route/route.go
@@ -9,4 +9,17 @@ import (
 func Route(route *gin.Engine) {
 	route.POST("/login", controller.Login)
 	route.POST("/register", controller.Register)
+	api := route.Use()
+	// ticket
+	api.POST("/ticket/create", controller.CreateTicket)
+	api.POST("/ticket/update/:id", controller.UpdateTicket)
+	api.DELETE("/ticket/delete/:id", controller.DeleteTicket)
+	// ticket type
+	api.POST("/ticket_type/create", controller.CreateTicketType)
+	api.POST("/ticket_type/update/:id", controller.UpdateTicketType)
+	api.DELETE("/ticket_type/delete/:id", controller.DeleteTicketType)
+	// ticket sale price
+	api.POST("/ticket_sale_price/create", controller.CreateTicketSalePrice)
+	api.POST("/ticket_sale_price/update/:id", controller.UpdateTicketSalePrice)
+	api.DELETE("/ticket_sale_price/delete/:id", controller.DeleteTicketSalePrice)
 }
