56952dce8b83eb822c8ec0004229ac125c3df488
zachary <zachary@gastom.com.tw>
2024-03-13 15:32:57 +0800

    fix: request has CORS problem.

--- route/route.go
+++ route/route.go
@@ -8,23 +8,27 @@ import (
 )
 
 func Route(route *gin.Engine) {
+	route.Use(middleware.CORS)
 	route.POST("/login", controller.Login)
 	route.POST("/register", controller.Register)
-	api := route.Use(middleware.JWTAuth)
-	// ticket
-	api.POST("/ticket/create", controller.CreateTicket)
-	api.POST("/ticket/update/:id", controller.UpdateTicket)
-	api.DELETE("/ticket/delete/:id", controller.DeleteTicket)
-	// ticket type
-	api.POST("/ticket_type/create", controller.CreateTicketType)
-	api.POST("/ticket_type/update/:id", controller.UpdateTicketType)
-	api.DELETE("/ticket_type/delete/:id", controller.DeleteTicketType)
-	// ticket sale price
-	api.POST("/ticket_sale_price/create", controller.CreateTicketSalePrice)
-	api.POST("/ticket_sale_price/update/:id", controller.UpdateTicketSalePrice)
-	api.DELETE("/ticket_sale_price/delete/:id", controller.DeleteTicketSalePrice)
-	// order
-	api.POST("/order/create", controller.CreateOrder)
-	api.POST("/order/update/:id", controller.UpdateOrder)
-	api.DELETE("/order/delete/:id", controller.DeleteOrder)
+	api := route.Group("/api")
+	api.Use(middleware.JWTAuth)
+	{
+		// ticket
+		api.POST("/ticket/create", controller.CreateTicket)
+		api.POST("/ticket/update/:id", controller.UpdateTicket)
+		api.DELETE("/ticket/delete/:id", controller.DeleteTicket)
+		// ticket type
+		api.POST("/ticket_type/create", controller.CreateTicketType)
+		api.POST("/ticket_type/update/:id", controller.UpdateTicketType)
+		api.DELETE("/ticket_type/delete/:id", controller.DeleteTicketType)
+		// ticket sale price
+		api.POST("/ticket_sale_price/create", controller.CreateTicketSalePrice)
+		api.POST("/ticket_sale_price/update/:id", controller.UpdateTicketSalePrice)
+		api.DELETE("/ticket_sale_price/delete/:id", controller.DeleteTicketSalePrice)
+		// order
+		api.POST("/order/create", controller.CreateOrder)
+		api.POST("/order/update/:id", controller.UpdateOrder)
+		api.DELETE("/order/delete/:id", controller.DeleteOrder)
+	}
 }
